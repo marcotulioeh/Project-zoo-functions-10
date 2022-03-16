0bee76d8421356bc4c8b943f26826d05a75b3dfe
Marco Túlio Huara <marcotuliohuara@gmail.com>
2022-03-16 17:34:26 -0300

    Adiciona requisito 3 :rocket:

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
-function getEmployeeByName(employeeName) {
-  // seu código aqui
-}
+const { employees } = data;
+
+const getEmployeeByName = (employeeName) => {
+  if (!employeeName) return {};
+  // eslint-disable-next-line max-len
+  return employees.find((element) => employeeName === element.firstName || employeeName === element.lastName);
+};
 
 module.exports = getEmployeeByName;
