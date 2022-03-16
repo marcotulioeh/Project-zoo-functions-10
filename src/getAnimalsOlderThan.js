ff527385af3b12d4b4e031afdf9650bfb28593ba
Marco Túlio Huara <marcotuliohuara@gmail.com>
2022-03-16 16:52:47 -0300

    Adiciona requisito 2 :rocket:

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,9 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
-}
+const getAnimalsOlderThan = (animal, ages) => {
+  const checkAgeMin = species.find(({ name }) => name === animal);
+  return checkAgeMin.residents.every((ageMin) => ageMin.age >= ages);
+};
 
 module.exports = getAnimalsOlderThan;
