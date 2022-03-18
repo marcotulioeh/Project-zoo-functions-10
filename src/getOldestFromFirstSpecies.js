f4b129e0ba7191e59364b690fa5300f035d88bbb
Marco Túlio Huara <marcotuliohuara@gmail.com>
2022-03-18 01:28:59 -0300

    Adiciona requisito 9 :rocket:

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
-function getOldestFromFirstSpecies(id) {
-  // seu código aqui
-}
+const { species, employees } = data;
+
+const getOldestFromFirstSpecies = (id) => {
+  const empy = employees.find((element) => element.id === id).responsibleFor[0];
+  const specie = species.find((element) => element.id === empy);
+  const arrOlder = specie.residents.reduce((acc, curr) => (curr.age > acc.age ? curr : acc));
+  return Object.values(arrOlder);
+};
 
 module.exports = getOldestFromFirstSpecies;
