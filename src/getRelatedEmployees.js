c89499ffe1144b59dada89c3d60cb60314d70564
Marco Túlio Huara <marcotuliohuara@gmail.com>
2022-03-18 01:47:16 -0300

    Adiciona correção com de linhas :rocket:

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -2,20 +2,15 @@ const data = require('../data/zoo_data');
 
 const { employees } = data;
 
-const stephanieId = '9e7d4524-363c-416a-8759-8aa7e50c0992';
-const olaId = 'fdb2543b-5662-46a7-badc-93d960fdc0a8';
-const burlId = '0e7b460e-acf4-4e17-bcb3-ee472265db83';
-const managers = [stephanieId, olaId, burlId];
-
-const isManager = (id) => managers.includes(id);
+const isManager = (id) => employees.some((element) => element.managers.includes(id));
 
 const getRelatedEmployees = (managerId) => {
   if (!isManager(managerId)) {
     throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
   }
   const nameEmployees = employees.filter((name) => name.managers.includes(managerId));
-  // eslint-disable-next-line max-len
-  return nameEmployees.map((collaborators) => `${collaborators.firstName} ${collaborators.lastName}`);
+  return nameEmployees.map((collaborators) =>
+    `${collaborators.firstName} ${collaborators.lastName}`);
 };
 
 module.exports = { isManager, getRelatedEmployees };
