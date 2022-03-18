0b3db196ec912d093ee3a1b6dc198bd60df52076
Marco Túlio Huara <marcotuliohuara@gmail.com>
2022-03-17 22:31:20 -0300

    Adiciona requisito 7 :rocket:

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -17,13 +17,13 @@ const countEntrants = (entrants) => entrants.reduce((acc, curr) => {
   return acc;
 }, { child: 0, adult: 0, senior: 0 });
 
-function calculateEntry(entrants) {
+const calculateEntry = (entrants) => {
   if (!entrants || Object.keys(entrants).length === 0) return 0;
   const func = countEntrants(entrants);
   const child = func.child * prices.child;
   const adult = func.adult * prices.adult;
   const senior = func.senior * prices.senior;
   return child + adult + senior;
-}
+};
 
 module.exports = { calculateEntry, countEntrants };
--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,63 @@
 const data = require('../data/zoo_data');
 
-function getAnimalMap(options) {
-  // seu código aqui
-}
+const { species } = data;
+
+const arrayAnimal = () => {
+  const mapLocation = species.map((loc) => loc.location);
+  return mapLocation.filter((loc, i) => mapLocation.indexOf(loc) === i);
+};
+
+const arrAniaml = arrayAnimal();
+
+const locAnimal = (locatinAnim) => {
+  const object = {};
+  locatinAnim.forEach((loc) => {
+    object[loc] = species.filter((anim) => anim.location === loc).map((nameAnim) => nameAnim.name);
+  });
+  return object;
+};
+
+const arrLocAnimal = locAnimal(arrAniaml);
+
+const arrayNameAnimal = (arrayLocations) => {
+  const object = {};
+  arrayLocations.forEach((loc) => {
+    object[loc] = species.filter((anim) => loc === anim.location).map((nameAnim) => {
+      const objctResult = {};
+      objctResult[nameAnim.name] = nameAnim.residents.map((res) => res.name);
+      return objctResult;
+    });
+  });
+  return object;
+};
+
+const arrNameAnim = arrayNameAnimal(arrAniaml);
+
+const nameSexAnimal = (arrayLocations, sex) => {
+  const result = {};
+  arrayLocations.forEach((loc) => {
+    result[loc] = species.filter((spc) => loc === spc.location).map((mapName) => {
+      const objResidents = {};
+      objResidents[mapName.name] = mapName.residents.filter((res) => res.sex === sex)
+        .map((res) => res.name);
+      return objResidents;
+    });
+  });
+  return result;
+};
+
+const getAnimalMap = ({ includeNames, sex, sorted } = 0) => {
+  if (!includeNames) return arrLocAnimal;
+  let result = arrNameAnim;
+  if (sex) {
+    result = nameSexAnimal(arrAniaml, sex);
+  }
+  if (sorted) {
+    arrAniaml.forEach((loc) => {
+      result[loc].forEach((element) => element[Object.keys(element)[0]].sort());
+    });
+  }
+  return result;
+};
 
 module.exports = getAnimalMap;
